c474588d112f2f6713b9a6c5874eda5c7529ad5d
Svetlana <cecastosic@gmail.com>
2022-04-24 22:48:53 +0200

    sort by date

--- src/App.tsx
+++ src/App.tsx
@@ -34,11 +34,12 @@ function App() {
         setFilteredData(
           filteredEvents
             .concat(filteredEventsPageTwo)
-            // .sort((a: any, b: any) => {
-            //   const dateB = new Date(b.start.utc).getTime();
-            //   const dateA = new Date(a.start.utc).getTime();
-            //   return dateB - dateA;
-            // })
+            .sort((a: any, b: any) => {
+              const dateB = new Date(b.start.utc).getTime();
+              const dateA = new Date(a.start.utc).getTime();
+              return dateB - dateA;
+            })
+            //to check better way to sort by date
         );
     }
   }, [eventsData, pageTwoData]);
